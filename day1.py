237e2321b2474ce4df86e715b84a0c78a257e106
siamahmed-user <66516210+siamahmed-user@users.noreply.github.com>
2020-11-21 10:40:46 +0600

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,15 @@
+def fun():
+    x = [1, 2, 3, 4]
+    y = ["hello" , "hi"]
+    for number in x:
+        print(number)
+    for string in y:
+        print(string)
+fun()
+class funti:
+    var = 12
+    start = input("anythin: ")
+    while var == 12:
+         print(19)
+
+com = funti()
